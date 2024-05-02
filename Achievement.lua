0b8b42608dbaddda85ea0acf8df48d90f4e7a5b4
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-02 16:15:06 +0800

    Create Achievement.lua

---
+++ Achievement.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Traumatic Beauty buffet Crowd",
+    Desc = "Hey Bro Look at me ",
+    Reason = "Survive the terrorist",
+    Image = "rbxassetid://17357468089"
+})
